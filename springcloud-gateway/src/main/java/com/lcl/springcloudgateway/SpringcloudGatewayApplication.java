5547c32324256502d1728c150ef1472b0befe3b6
lcl123321 <15272502890@163.com>
2023-04-08 10:54:14 +0800

    2023.4.8 gateway网关

---
+++ springcloud-gateway/src/main/java/com/lcl/springcloudgateway/SpringcloudGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.lcl.springcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringcloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGatewayApplication.class, args);
+    }
+
+}
